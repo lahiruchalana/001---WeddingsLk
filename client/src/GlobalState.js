58a59f0ee27b18fb0065afd9e6506846b2e9f1ac
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-07-25 10:07:11 +0530

    first commit

---
+++ client/src/GlobalState.js
@@ -0,0 +1,49 @@
+import React, {createContext, useState, useEffect} from 'react'
+import ProductsAPI from './api/ProductsAPI'
+import UserAPI from './api/UserAPI'
+import CategoriesAPI from './api/CategoriesAPI'
+import UserInfoAPI from './api/UserInfoAPI'
+import WeddingPlansAPI from './api/WeddingPlansAPI'
+import axios from 'axios'
+
+export const GlobalState = createContext()
+
+
+export const DataProvider = ({children}) =>{
+    const [token, setToken] = useState(false)
+
+
+    useEffect(() =>{
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin){
+            const refreshToken = async () =>{
+                const res = await axios.get('/user/refresh_token')
+        
+                setToken(res.data.accesstoken)
+    
+                setTimeout(() => {
+                    refreshToken()
+                }, 10 * 60 * 1000)
+            }
+            refreshToken()
+        }
+    },[])
+
+
+    
+    const state = {
+        token: [token, setToken],
+        productsAPI: ProductsAPI(),
+        userAPI: UserAPI(token),
+        categoriesAPI: CategoriesAPI(),
+        userInfoAPI: UserInfoAPI(),
+        weddingPlansAPI: WeddingPlansAPI()
+
+    }
+
+    return (
+        <GlobalState.Provider value={state}>
+            {children}
+        </GlobalState.Provider>
+    )
+}
