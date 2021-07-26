302f2ffd425ba3989a0469765918d95bd4db344b
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-07-27 01:27:50 +0530

    nice states created

--- client/src/components/adminProfile/AdminProfile.js
+++ client/src/components/adminProfile/AdminProfile.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import SideBarAdmin from "./SideBarAdmin";
 import Header from "../header/Header";
 
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {GlobalState} from '../../GlobalState'
 import UserItem from '../mainpages/utils/userItem/UserItem'
 import Loading from '../mainpages/utils/loading/Loading'
@@ -14,6 +14,10 @@ import LoadMore from './LoadMore'
 import Button from '@material-ui/core/Button';
 // import PaypalButton from './PaypalButton'
 
+import Aos from "aos";
+import 'aos/dist/aos.css';
+
+
 function AdminProfile() {
 
     const state = useContext(GlobalState)
@@ -23,6 +27,10 @@ function AdminProfile() {
     const [callback, setCallback] = state.userInfoAPI.callback
     const [loading, setLoading] = useState(false)
     const [isCheck, setIsCheck] = useState(false)
+    const [totalCustomers, setTotalCustomers] = useState(0)
+    const [totalCompleted, setTotalCompleted] = useState(0)
+    const [totalProgressIn, setTotalProgressIn] = useState(0)
+    const [totalNotAssigned, setTotalNotAssigned] = useState(0)
 
     const handleCheck = (id) =>{
         users.forEach(user => {
@@ -30,6 +38,40 @@ function AdminProfile() {
         })
         setUsers([...users])
     }
+
+    useEffect(() =>{
+        const getTotal = () =>{
+            const totalCustomers = users.reduce((prev, user) => {
+                return  user.progress == '0' || user.progress == '1'|| user.progress == '2' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalCustomers(totalCustomers)
+
+            const totalCompleted = users.reduce((prev, user) => {
+                return  user.progress == '1' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalCompleted(totalCompleted)
+
+            const totalProgressIn = users.reduce((prev, user) => {
+                return  user.progress == '2' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalProgressIn(totalProgressIn)
+
+            const totalNotAssigned = users.reduce((prev, user) => {
+                return  user.progress == '0' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalNotAssigned(totalNotAssigned)
+
+        }
+
+        Aos.init({ duration: 2500 });
+
+        getTotal()
+
+    },[users])
 
     // const deleteProduct = async(id, public_id) => {
     //     try {
@@ -93,6 +135,25 @@ function AdminProfile() {
         } */}
 
         <Text4>Customer Details</Text4>
+        <Product_card data-aos="fade-left">
+            <Text9>Number of Total Customer</Text9>
+            <Text10>{totalCustomers}</Text10>
+        </Product_card>
+
+        <Product_card1 data-aos="fade-left">
+            <Text9>Number of Weddings not Assigned</Text9>
+            <Text10>{totalNotAssigned}</Text10>
+        </Product_card1>
+
+        <Product_card2 data-aos="fade-left">
+            <Text9>Number of Progress in Weddings</Text9>
+            <Text10>{totalProgressIn}</Text10>
+        </Product_card2>
+
+        <Product_card3 data-aos="fade-left">
+            <Text9>Number of Completed Weddings</Text9>
+            <Text10>{totalCompleted}</Text10>
+        </Product_card3>
 
         <div className="products">
             {
@@ -136,5 +197,79 @@ const Text4 = styled.div`
     font-weight: 700;
     text-transform: uppercase;
 `;
+
+const Text9 = styled.div`
+    font-size: 12px;
+    color: white;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+const Text10 = styled.div`
+    margin-left: auto;
+    margin-top: auto;
+    font-size: 35px;
+    color: white;
+    font-weight: 700;
+    text-transform: uppercase;
+    width: auto;
+    text-align: center;
+    margin-top: 5px;
+`;
+
+const Product_card = styled.div`
+  width: 700px;
+  background-color: #370617;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px red;
+  position: relative;
+  left: 320px;
+  margin-top: -25px;
+  align-items: center;
+`;
+
+const Product_card1 = styled.div`
+  width: 300px;
+  background-color: #bf0603;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px black;
+  position: relative;
+  left: 780px;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+const Product_card2 = styled.div`
+  width: 300px;
+  background-color: #ffba08;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px black;
+  position: relative;
+  left: 440px;
+  margin-top: -100px;
+  align-items: center;
+`;
+
+const Product_card3 = styled.div`
+  width: 300px;
+  background-color: #70e000;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px black;
+  position: relative;
+  left: 100px;
+  margin-top: -100px;
+  align-items: center;
+`;
+
 
 export default AdminProfile;
--- client/src/components/adminProfile/CurrentEmployees.js
+++ client/src/components/adminProfile/CurrentEmployees.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import SideBarAdmin from "./SideBarAdmin";
 import Header from "../header/Header";
 
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {GlobalState} from '../../GlobalState'
 import UserItem from '../mainpages/utils/userItem/UserItem'
 import Loading from '../mainpages/utils/loading/Loading'
@@ -14,6 +14,9 @@ import LoadMore from './LoadMore'
 import Button from '@material-ui/core/Button';
 // import PaypalButton from './PaypalButton'
 
+import Aos from "aos";
+import 'aos/dist/aos.css';
+
 function AdminProfile() {
 
     const state = useContext(GlobalState)
@@ -23,6 +26,9 @@ function AdminProfile() {
     const [callback, setCallback] = state.userInfoAPI.callback
     const [loading, setLoading] = useState(false)
     const [isCheck, setIsCheck] = useState(false)
+    const [totalAdmin, setTotalAdmin] = useState(0)
+    const [totalEmployee, setTotalEmployee] = useState(0)
+
 
     const handleCheck = (id) =>{
         users.forEach(user => {
@@ -30,6 +36,28 @@ function AdminProfile() {
         })
         setUsers([...users])
     }
+
+    useEffect(() =>{
+        const getTotal = () =>{
+            const totalAdmin = users.reduce((prev, user) => {
+                return  user.role == '1' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalAdmin(totalAdmin)
+
+            const totalEmployee = users.reduce((prev, user) => {
+                return  user.role == '2' ? prev + 1 : prev + 0
+            },0)
+
+            setTotalEmployee(totalEmployee)
+
+        }
+
+        Aos.init({ duration: 2500 });
+
+        getTotal()
+
+    },[users])
 
     // const deleteProduct = async(id, public_id) => {
     //     try {
@@ -91,8 +119,14 @@ function AdminProfile() {
                 <button onClick={deleteAll}>Delete ALL</button>
             </div>
         } */}
-
+        <div >
         <Text4>Current Employees</Text4>
+        <Product_card data-aos="fade-left">
+            <Text9>Number of Working Employees</Text9>
+            <Text10>{totalEmployee}</Text10>
+        </Product_card>
+        </div>
+
         
         <div className="products">
             {
@@ -109,6 +143,11 @@ function AdminProfile() {
 
 
         <Text4>Admin Info</Text4>
+        <Product_card1 data-aos="fade-left">
+            <Text9>Number of Admins</Text9>
+            <Text10>{totalAdmin}</Text10>
+        </Product_card1>
+
         
         <div className="products">
             {
@@ -149,6 +188,53 @@ const Text4 = styled.div`
     color: rgb(78, 6, 69);
     font-weight: 700;
     text-transform: uppercase;
+`;
+
+const Text9 = styled.div`
+    font-size: 12px;
+    color: white;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+const Text10 = styled.div`
+    margin-left: auto;
+    margin-top: auto;
+    font-size: 35px;
+    color: white;
+    font-weight: 700;
+    text-transform: uppercase;
+    width: auto;
+    text-align: center;
+    margin-top: 5px;
+`;
+
+const Product_card = styled.div`
+  width: 600px;
+  background-color: #370617;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px black;
+  position: relative;
+  left: 430px;
+  margin-top: -25px;
+  align-items: center;
+`;
+
+const Product_card1 = styled.div`
+  width: 600px;
+  background-color: #10002b;
+  overflow: hidden;
+  height: 100px;
+  padding: 15px;
+  box-shadow: 0 0 15px black;
+  position: relative;
+  left: 430px;
+  margin-top: -25px;
+  align-items: center;
 `;
 
 export default AdminProfile;
